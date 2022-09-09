9024ce997b635ed953e236417494618aea85be33
okolieblessing <okolieblessing419@gmail.com>
2022-09-08 17:16:53 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**                                                                                                                                     
  * main - Serves as the entry point for the program
  *
- * Return: By default returns zero if no erros otherwise return non-zero value.                                                         
- */                                                                                                                                     
+ * Return: By default returns zero if no erros otherwise return non-zero value.
+ */
 int main(void)
-{                                                                                                                      
-        puts("\"Programming is like building a multilingual puzzle"); 
-        return (0);                                                                                                                     
+{
+       	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
